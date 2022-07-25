5ed3c028d18b53e5bf25f26e20f54c9d09d14426
OverTek22 <109820719+OverTek22@users.noreply.github.com>
2022-07-25 14:52:10 -0700

    Update mbta.py
    Updating base url to use Trimets api?

--- mbta.py
+++ mbta.py
@@ -59,7 +59,7 @@ class MBTA():
     self.serverError = False
      
     # base url
-    api_url = "https://api-v3.mbta.com/{}".format(endPoint)
+    api_url = "https://developer.trimet.org/ws/V1/arrivals/locIDs/6849,6850/appID/{}".format(endPoint)
 
     # if we are using an api key and have args
     if self.apiKey != None and args != None:
